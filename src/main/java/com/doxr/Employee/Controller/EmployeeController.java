e6ff9766c0911c40e745bfc53d9d0251f7a5b1b4
Basude <basudevmukh@gmail.com>
2026-08-01 01:08:45 +0530

    Initial Commit

---
+++ src/main/java/com/doxr/Employee/Controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.doxr.Employee.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+
+    @GetMapping("/joinName")
+    public String getName(@RequestParam String firstName,
+                           @RequestParam String lastName) {
+        return firstName + " " + lastName;
+    }
+
+}
